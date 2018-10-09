72819f000f86cfdc1d11220c84b95b0fe040a9a5
wangyang <wangyang06@finupgroup.com>
2018-10-09 16:58:43 +0800

    增加一点注释

--- build/config.js
+++ build/config.js
@@ -9,10 +9,10 @@ const config = {
   production: {
     port: 8071,
     proxy: {
-      '/api': 'http://localhost:3002',
-      '/contract': 'http://106.75.9.81'
+      '/api': 'http://localhost:3002'
     }
   },
+  // 如果通过ui界面更改了配置后，这里要一致
   assetsDir: 'static',
   outputDir: 'dest'
 };
